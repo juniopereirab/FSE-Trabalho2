777d46198977cd344ddf5224f246bd271c0cc496
Dâmaso <junio.pereirab@gmail.com>
2022-08-28 22:47:15 -0300

    Adiciona funções para o Display

---
+++ Trabalho/inc/pid.h
@@ -0,0 +1,8 @@
+#ifndef PID_H_
+#define PID_H_
+
+void pidSetupConstants(double Kp_, double Ki_, double Kd_);
+void pidUpdateReference(float reference_);
+double pidControl(double saida_medida);
+
+#endif /* PID_H_ */
--- Trabalho/src/main.c
+++ Trabalho/src/main.c
@@ -7,19 +7,58 @@
 
 #include "pid.h"
 #include "gpio.h"
+#include "uart.h"
+#include "display.h"
+
+int uart_filestream, key_gpio = 1;
+// struct bme280_dev bme_connection;
+
+void exitProgram(){
+    system("clear");
+    printf("Programa encerrado!\n");
+    turnResistanceOff();
+    turnFanOff();
+    closeUart(uart_filestream);
+    exit(0);
+}
 
 void PID() {
     system("clear");
-    float hys, TI, TR, TE;
+    float TI, TR, TE;
     printf("\n██████████████████ Método PID ██████████████████\n");
     pidSetupConstants(30, 0.2, 400);
     
-    
+    requestToUart(uart_filestream, GET_INTERNAL_TEMP);
+    TI = readFromUart(uart_filestream, GET_INTERNAL_TEMP).int_value;
+    printf("%f\n", TI);
+    double value = pidControl(TI);
+    //pwmControl(value);
+    printf("%lf\n", value);
 
+    requestToUart(uart_filestream, GET_POTENTIOMETER);
+    TR = readFromUart(uart_filestream, GET_POTENTIOMETER).float_value;
+    printf("%f\n", TR);
+    pidUpdateReference(TR);
+
+    TE = 0;
+    printDisplay(TI, TR, TE);
+}
+
+void startProgram(){
+    wiringPiSetup();
+    // turnResistanceOff();
+    // turnFanOff();
+    connectDisplay();
+    // bme_connection = connectBme();
+    uart_filestream = initUart();
 }
 
 int main () {
-    if(wiringPiSetup() == -1) return 1;
+
+    signal(SIGINT, exitProgram);
+
+    startProgram();
+
     PID();
 
     return 0;
