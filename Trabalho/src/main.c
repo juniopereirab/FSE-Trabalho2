83f0ae02762e37c2f39135269ec5889a2e3fd6cf
Dâmaso Júnio <junio.pereirab@gmail.com>
2022-08-30 14:45:06 -0300

    Adiciona tratamento de temperatura atual

--- Trabalho/src/main.c
+++ Trabalho/src/main.c
@@ -6,12 +6,14 @@
 #include <unistd.h>
 
 #include "pid.h"
+#include "bme280.h"
 #include "gpio.h"
 #include "uart.h"
 #include "display.h"
+#include "temperature.h"
 
 int uart_filestream, key_gpio = 1;
-// struct bme280_dev bme_connection;
+struct bme280_dev bme_connection;
 
 void exitProgram(){
     system("clear");
@@ -27,21 +29,35 @@ void PID() {
     float TI, TR, TE;
     printf("\n██████████████████ Método PID ██████████████████\n");
     pidSetupConstants(30, 0.2, 400);
-    
-    requestToUart(uart_filestream, GET_INTERNAL_TEMP);
-    TI = readFromUart(uart_filestream, GET_INTERNAL_TEMP).int_value;
-    printf("%f\n", TI);
-    double value = pidControl(TI);
-    //pwmControl(value);
-    printf("%lf\n", value);
+    do {
+        requestToUart(uart_filestream, GET_INTERNAL_TEMP);
+        TI = readFromUart(uart_filestream, GET_INTERNAL_TEMP).float_value;
+        printf("%f\n", TI);
+        double value = pidControl(TI);
+        pwmControl(value);
+        printf("%lf\n", value);
 
-    requestToUart(uart_filestream, GET_POTENTIOMETER);
-    TR = readFromUart(uart_filestream, GET_POTENTIOMETER).float_value;
-    printf("%f\n", TR);
-    pidUpdateReference(TR);
+        requestToUart(uart_filestream, GET_POTENTIOMETER);
+        TR = readFromUart(uart_filestream, GET_POTENTIOMETER).float_value;
+        printf("%f\n", TR);
+        pidUpdateReference(TR);
 
-    TE = 0;
-    printDisplay(TI, TR, TE);
+        TE = getCurrentTemperature(&bme_connection);
+        printf("\tTI: %.2f⁰C - TR: %.2f⁰C - TE: %.2f⁰C\n", TI, TR, TE);
+        printDisplay(TI, TR, TE);
+
+        if(TR >= TI){
+            turnResistanceOn(100);
+            turnFanOff();
+            value = 100;
+        }
+        else if(TR <= TI){
+            turnFanOn(100);
+            turnResistanceOff();
+            value = -100;
+        }
+
+    } while (1);
 }
 
 void startProgram(){
@@ -49,15 +65,15 @@ void startProgram(){
     // turnResistanceOff();
     // turnFanOff();
     connectDisplay();
-    // bme_connection = connectBme();
+    bme_connection = connectBme();
     uart_filestream = initUart();
 }
 
 int main () {
 
+    startProgram();
     signal(SIGINT, exitProgram);
 
-    startProgram();
 
     PID();
 
